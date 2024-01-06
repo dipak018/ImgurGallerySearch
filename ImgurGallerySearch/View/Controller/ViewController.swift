35e21c56abda0bf4e9b8773d04cf8de75bdea6c3
Dipak Bharat Chaudhari <dipak.bharat.chaudhari@sap.com>
2024-01-06 13:47:25 +0530

    Initial Commit
    - Uploading the basic folder structure with MVVM pattern

---
+++ ImgurGallerySearch/View/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImgurGallerySearch
+//
+//  Created by Chaudhari, Dipak Bharat (external - Service) on 06/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
