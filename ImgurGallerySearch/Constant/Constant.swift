03d025a1e31769cde0e405400f2936fa8b6ddec1
Dipak Bharat Chaudhari <dipak.bharat.chaudhari@sap.com>
2024-01-08 15:53:27 +0530

    Changes the sorting by time for getting result in reverse chronological order

--- ImgurGallerySearch/Constant/Constant.swift
+++ ImgurGallerySearch/Constant/Constant.swift
@@ -9,6 +9,6 @@ enum Constant {
     enum API {
         static let clientId = "724611fb9e0c178"
         static let supportedImageTypes = "jpg,jpeg,png"
-        static let gallerySearchURL = "https://api.imgur.com/3/gallery/search/top/week"
+        static let gallerySearchURL = "https://api.imgur.com/3/gallery/search/time/week"
     }
 }
